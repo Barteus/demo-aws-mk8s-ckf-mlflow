fcd0f491830d04e8423267ae00d739d49877168b
Bartlomiej Poniecki-Klotz <b.poniecki.klotz@gmail.com>
2024-09-17 22:22:46 +0200

    chatbot UI added

---
+++ ui/app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import os
+
+from langchain_community.vectorstores import OpenSearchVectorSearch
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.globals import set_verbose, set_debug
+
+from langchain_community.llms import VLLMOpenAI
+
+import logging
+
+logging.getLogger().setLevel(logging.ERROR)
+set_verbose(False)
+set_debug(False)
+
+def embeddings(model_name= "sentence-transformers/all-MiniLM-L6-v2", use_gpu=False):
+    hfe = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs={"device": "cuda" if use_gpu else "cpu"},
+    )
+    return hfe
+
+def vector_search(
+    os_host, os_port, os_user, os_user_pass, 
+    embedding_function, os_index_name = "pagedata_index"
+):
+    opensearch_vector_search = OpenSearchVectorSearch(
+        opensearch_url = f"https://{os_host}:{os_port}",
+        index_name = os_index_name,
+        embedding_function = embedding_function,
+        http_auth = (os_user,os_user_pass),
+        verify_certs = False,
+        ssl_assert_hostname = False,
+        ssl_show_warn = False
+    )
+    return opensearch_vector_search
+
+hfe = embeddings()
+    
+vector_search = vector_search(
+    os_host=os.getenv('OPENSEARCH_HOST', "34.245.99.62"),
+    os_port=os.getenv('OPENSEARCH_PORT', 9200),
+    os_user=os.getenv('OPENSEARCH_USER', "admin"),
+    os_user_pass=os.getenv('OPENSEARCH_PASSWORD', "admin"),
+    embedding_function=hfe
+)
+
+llm = VLLMOpenAI(
+    openai_api_key="EMPTY",
+    openai_api_base=os.getenv('LLM_API_URL', "http://10.152.183.161/v1/"),
+    model_name=os.getenv('LLM_MODEL_NAME', "meta/llama3-8b-instruct"),
+    max_tokens=512
+    
+)
+
+st.title("Canonical and NVidia RAG Demo with NIMs", anchor=False)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if question := st.chat_input("Ask your question!", key="chatbot_main_area"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(question)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": question})
+
+    # # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(llm.stream(question))
+    # Add assistant response to chat history
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response}
+    )
+
--- ui/setup.sh
+++ ui/setup.sh
@@ -1,3 +1,66 @@
 #!/bin/bash
 
-# TBD
+kubectl apply -f - <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: chatbot-deployment
+  labels:
+    app: chatbot
+spec:
+  selector:
+    matchLabels:
+      app: chatbot
+  template:
+    metadata:
+      labels:
+        app: chatbot
+    spec:
+      containers:
+      - name: ui
+        image: bponieckiklotz/llm-chatbot:kserve-v1
+        ports:
+        - containerPort: 8501
+        env:
+        - name: OPENSEARCH_HOST
+          valueFrom:
+            secretKeyRef:
+              name: opensearch-secret
+              key: host
+              optional: false
+        - name: OPENSEARCH_PORT
+          valueFrom:
+            secretKeyRef:
+              name: opensearch-secret
+              key: port
+              optional: false
+        - name: OPENSEARCH_USER
+          valueFrom:
+            secretKeyRef:
+              name: opensearch-secret
+              key: username
+              optional: false
+        - name: OPENSEARCH_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: opensearch-secret
+              key: password
+              optional: false
+        - name: LLM_API_URL
+          value: "http://llama3-8b-instruct-1xgpu-predictor-00001-private/v1"
+        - name: LLM_MODEL_NAME
+          value: "meta/llama3-8b-instruct"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: chatbot-service
+spec:
+  selector:
+    app: chatbot
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 8501
+  type: LoadBalancer
+EOF
